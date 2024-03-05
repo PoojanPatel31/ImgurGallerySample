caa1a91ab030a439dff8d73449d5d6660b4f24e2
Poojan Patel <ppoojan92@gmail.com>
2024-03-05 17:24:29 +0530

    Use DI to provide dependency to worker class.
    - Create custom worker factory provider class.

--- app/src/main/java/com/imgurgallery/GalleryApplication.kt
+++ app/src/main/java/com/imgurgallery/GalleryApplication.kt
@@ -2,11 +2,14 @@ package com.imgurgallery
 
 import android.app.Application
 import android.content.Context
-import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import com.imgurgallery.db.repo.GalleryRepository
 import com.imgurgallery.managers.WorkManager
+import com.imgurgallery.worker.GalleryWorker
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -17,14 +20,24 @@ class GalleryApplication : Application(), Configuration.Provider {
     }
 
     @Inject
-    lateinit var workerFactory: HiltWorkerFactory
+    lateinit var customWorkerFactory: CustomWorkerFactory
 
     override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder().setWorkerFactory(workerFactory).build()
+        get() = Configuration.Builder().setWorkerFactory(customWorkerFactory).build()
 
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
         WorkManager.setupWorker(appContext)
+    }
+
+    class CustomWorkerFactory @Inject constructor(private val repo: GalleryRepository) :
+        WorkerFactory() {
+        override fun createWorker(
+            appContext: Context,
+            workerClassName: String,
+            workerParameters: WorkerParameters
+        ): ListenableWorker = GalleryWorker(appContext, workerParameters, repo)
+
     }
 }
--- app/src/main/java/com/imgurgallery/worker/GalleryWorker.kt
+++ app/src/main/java/com/imgurgallery/worker/GalleryWorker.kt
@@ -2,16 +2,24 @@ package com.imgurgallery.worker
 
 import android.content.Context
 import android.util.Log
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.imgurgallery.db.repo.GalleryRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
-class GalleryWorker(private val context: Context, params: WorkerParameters) :
-    CoroutineWorker(context, params) {
+
+@HiltWorker
+class GalleryWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    private var galleryRepository: GalleryRepository
+) : CoroutineWorker(context, params) {
+
     override suspend fun doWork(): Result {
         Log.e("GalleryWorker", "Fetching and storing in background.")
-        val repo = GalleryRepository(context)
-        repo.fetchAndCacheGallery()
+        galleryRepository.fetchAndCacheGallery()
         Log.e("GalleryWorker", "Fetching and storing in background completed")
         Log.e("GalleryWorker", "-------------------")
         return Result.success()
